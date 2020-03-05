f10b3defdc96000c56bcb1b66ccb797ee15a30dd
Vishal1003 <vjha10032001@gmail.com>
2020-03-06 00:05:29 +0530

    Day2

--- Arrays/Arrays1.java
+++ Arrays/Arrays1.java
@@ -4,10 +4,15 @@ public class Arrays1 {
 
 	public static void main(String[] args) {
 
-		int a[] = { 10, 20, 50, 80, 930, 70,30 };
+		int a[] = { 1, 2, 3, 10, 88, 12, 33 };
 //		maxElement(a);
 //		maxIndex(a);
-		ArrayReversal(a);
+//		ArrayReversal(a);
+//		AraayInverse(a);
+		bubbleSort(a);
+		selectionSort(a);
+		insertionSort(a);
+
 	}
 
 //	Maximum in Array:
@@ -57,13 +62,96 @@ public class Arrays1 {
 			arr[i] = arr[size - 1 - i];
 			arr[size - 1 - i] = temp;
 		}
-		
-		for(int val: arr) {
-			System.out.print(val+"  ");
+
+		for (int val : arr) {
+			System.out.print(val + "  ");
 		}
 		System.out.println();
 	}
-	
-//	Inverted Array
 
+//	Inverted Array
+	public static void AraayInverse(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[arr[i]] = i + 1;
+		}
+
+		for (int val : arr) {
+			System.out.print(val + " ");
+		}
+	}
+
+// isMirror Inverse
+	public static void isMirror(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[arr[i]] != i) {
+				System.out.println("False");
+				return;
+			}
+		}
+
+		System.out.println("True");
+	}
+
+//	bubbleSort
+	public static void bubbleSort(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j + 1];
+					arr[j + 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for (int val : arr) {
+			System.out.print(val + " ");
+		}
+		System.out.println();
+		System.out.println("****************************");
+
+	}
+
+	public static void selectionSort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int min = i;
+			for (int j = i; j < arr.length; j++) {
+				if (arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+
+		}
+		for (int val : arr) {
+			System.out.print(val + " ");
+		}
+		System.out.println();
+		System.out.println("****************************");
+
+	}
+
+	public static void insertionSort(int[] arr) {
+
+		for (int counter = 1; counter < arr.length; counter++) {
+			int temp = arr[counter];
+			int j = counter - 1;
+			while (j >= 0 && arr[j] > temp) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = temp;
+		}
+
+		for (int val : arr) {
+			System.out.print(val + " ");
+		}
+		System.out.println();
+		System.out.println("****************************");
+
+	}
 }
