2029f591383dd8fb9f065686590cd634e31e85ab
Vishal1003 <vjha10032001@gmail.com>
2020-03-10 20:45:52 +0530

    DAY 6

--- codeChef/LAZER_new.java
+++ codeChef/LAZER_new.java
@@ -2,9 +2,44 @@ package codeChef;
 
 import java.util.Scanner;
 
-public class LAZER_new {
+class Point {
+	double x, y;
 
-	public static void main(String[] args) {
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	static void displayPoint(Point p) {
+		System.out.println("(" + p.x + ", " + p.y + ")");
+	}
+}
+
+class Test {
+	static Point lineLineIntersection(Point A, Point B, Point C, Point D) {
+
+		double a1 = B.y - A.y;
+		double b1 = A.x - B.x;
+		double c1 = a1 * (A.x) + b1 * (A.y);
+
+		double a2 = D.y - C.y;
+		double b2 = C.x - D.x;
+		double c2 = a2 * (C.x) + b2 * (C.y);
+
+		double determinant = a1 * b2 - a2 * b1;
+
+		if (determinant == 0) {
+
+			return new Point(Double.MAX_VALUE, Double.MAX_VALUE);
+		} else {
+			double x = (b2 * c1 - b1 * c2) / determinant;
+			double y = (a1 * c2 - a2 * c1) / determinant;
+			return new Point(x, y);
+		}
+	}
+
+	public static void main(String args[]) {
+
 		Scanner scn = new Scanner(System.in);
 
 		int tc = 0;
@@ -34,9 +69,40 @@ public class LAZER_new {
 				int x2 = scn.nextInt();
 				int y = scn.nextInt();
 
-				for (int j = x1; j < x2-1; j++) {
-					if (y >= arr[j] && y <= arr[j]) {
-						count++;
+				for (int i = 0; i < arr.length - 1; i++) {
+					Point p1 = new Point(x1, y);
+					Point q1 = new Point(x2, y);
+					Point p2 = new Point(i + 1, arr[i]);
+					Point q2 = new Point(i + 2, arr[i + 1]);
+					Point intersection = lineLineIntersection(p1, q1, p2, q2);
+
+					if (p1.x == p2.x && p1.y == p2.y) {
+						if (q2.x <= p2.x)
+							continue;
+					}
+
+					if (p1.x == q2.x && p1.y == q2.y) {
+						if (q2.x >= p2.x)
+							continue;
+					}
+
+					if (q1.x == p2.x && q1.y == p2.y) {
+						if (q2.x >= p2.x)
+							continue;
+					}
+
+					if (q1.x == q2.x && q1.y == q2.y) {
+						if (q2.x <= p2.x)
+							continue;
+					}
+
+					if (intersection.x == Double.MAX_VALUE && intersection.y == Double.MAX_VALUE) {
+						continue;
+					}
+
+					else {
+						if (x1 <= intersection.x && x2 >= intersection.x && y == intersection.y)
+							count++;
 					}
 				}
 
@@ -50,5 +116,4 @@ public class LAZER_new {
 		}
 
 	}
-
 }
--- codeChef/LAZERnew.java
+++ codeChef/LAZERnew.java
@@ -29,53 +29,32 @@ public class LAZERnew {
 			while (q > 0) {
 
 				int count = 0;
-				
+
 				int x1 = scn.nextInt();
 				int x2 = scn.nextInt();
 				int y = scn.nextInt();
 
 				for (int i = 0; i < arr.length - 1; i++) {
-					int x = 0;
 
-					if ((arr[i + 1] >= y && arr[i] <= y) || (arr[i + 1] <= y && arr[i] >= y)) {
+					double x = 0.0;
 
-						if (((i + 1) == x2 && arr[i] == y)) {
-							if ((i + 2) > (i + 1))
-								continue;
-						}
-						if (((i + 2) == x2 && arr[i + 1] == y)) {
-							if ((i + 2) < (i + 1))
-								continue;
-						}
+					if (Math.max(arr[i + 1], arr[i]) >= y && Math.min(arr[i + 1], arr[i]) <= y) {
 
-						if (((i + 2) == x1) && arr[i + 1] == y) {
-							if ((i + 1) < (i + 2))
-								continue;
-						}
-						
-						if (((i + 1) == x1) && arr[i] == y) {
-							if ((i + 1) > (i + 2))
-								continue;
-						}
+						int slope = arr[i + 1] - arr[i];
+						int constant = (2 + i) * arr[i] - (1 + i) * arr[i + 1];
 
-						int m = arr[i + 1] - arr[i];
+						if (slope != 0) {
+							x = (y - constant) / slope;
 
-						int c = (i + 2) * arr[i] - (i + 1) * arr[i + 1];
-
-						if (m != 0) {
-							x = (y - c) / m;
-							if ((x1 <= x && x2 >= x) || (x2 <= x && x1 <= x)) {
+							if (Math.max(i + 1, i + 2) <= x2 && Math.min(i + 1, i + 2) >= x1)
 								count++;
-							}
 						} else {
-							if (((i + 1) <= x1 && (i + 2) > x1) || ((i + 2) <= x1 && (i + 1) > x1)
-									|| ((i + 1) >= x1 && (i + 2) <= x1) || (i + 2 >= x2 && i + 1 < x2)
-									|| (i + 1 >= x2 && i + 2 < x2)) {
+							if (Math.max(i + 1, i + 2) <= x2 && Math.min(i + 1, i + 2) >= x1)
 								count++;
-							}
 						}
 
 					}
+
 				}
 
 				System.out.println(count);
