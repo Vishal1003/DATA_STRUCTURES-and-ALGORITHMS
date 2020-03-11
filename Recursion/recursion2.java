1526ce234fd6a474fe460ce46941de95e4eb1352
Vishal1003 <vjha10032001@gmail.com>
2020-03-11 23:37:56 +0530

    DAY 7

--- Recursion/recursion2.java
+++ Recursion/recursion2.java
@@ -1,12 +1,17 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class recursion2 {
 	public static void main(String[] args) {
 
-		int[] arr = { 10, 20, 30, 40, 50,80,20,90 };
+		int[] arr = { 10, 20, 30, 20, 50, 80, 20, 90 };
 		int item = 20;
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+
 //		System.out.println(first_index(arr, item, 0));
-		System.out.println(last_index(arr, item, arr.length-1));
+//		System.out.println(last_index(arr, item, arr.length - 1));
+		System.out.println(all_indices(arr, item, 0, ans));
 	}
 
 	public static int first_index(int[] arr, int item, int i) {
@@ -34,4 +39,20 @@ public class recursion2 {
 		return last_index(arr, item, i - 1);
 
 	}
+
+	public static ArrayList<Integer> all_indices(int[] arr, int item, int i, ArrayList<Integer> ans) {
+
+		if (i == arr.length) {
+			return ans;
+		}
+
+		if (arr[i] == item) {
+			ans.add(i);
+		}
+
+		return all_indices(arr, item, i + 1, ans);
+
+	}
+
+	
 }
