a16a9a95c886699a205f462255ee35d993774085
Vishal1003 <vjha10032001@gmail.com>
2020-03-12 00:18:51 +0530

    DAY 8

--- Recursion/recursion2.java
+++ Recursion/recursion2.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 public class recursion2 {
 	public static void main(String[] args) {
 
-		int[] arr = { 10, 20, 30, 20, 50, 80, 20, 90 };
+		int[] arr = { 10, 20, 30, 40, 30, 60, 10 };
 		int item = 20;
 		ArrayList<Integer> ans = new ArrayList<Integer>();
 
 //		System.out.println(first_index(arr, item, 0));
 //		System.out.println(last_index(arr, item, arr.length - 1));
-		System.out.println(all_indices(arr, item, 0, ans));
+//		System.out.println(all_indices(arr, item, 0, ans));
+
+		System.out.println(isPalindrome(arr, 0, arr.length - 1));
+
 	}
 
 	public static int first_index(int[] arr, int item, int i) {
@@ -54,5 +57,17 @@ public class recursion2 {
 
 	}
 
-	
+	public static boolean isPalindrome(int[] arr, int l, int r) {
+		if (l > r) {
+			return true;
+		}
+
+		if (arr[l] != arr[r]) {
+			return false;
+		}
+
+		return isPalindrome(arr, l + 1, r - 1);
+
+		
+	}
 }
