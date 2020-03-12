8e048b2d82cb962792309b274117bc46e4fcdb12
Vishal1003 <vjha10032001@gmail.com>
2020-03-12 23:27:51 +0530

    DAY 7

--- Recursion/recursion3.java
+++ Recursion/recursion3.java
@@ -2,9 +2,10 @@ package Recursion;
 
 public class recursion3 {
 	public static void main(String[] args) {
-		String str = "ab23cd3f4";
+		String str = "123456";
 
-		System.out.println(printSum(str, 0, 0));
+//		System.out.println(printSum(str, 0, 0));
+		System.out.println(convertInt(str, 0, str.length()-1, 1));
 	}
 
 	public static int printSum(String str, int sum, int i) {
@@ -16,5 +17,15 @@ public class recursion3 {
 		}
 
 		return printSum(str, sum, i + 1);
+	}
+
+	public static int convertInt(String str, int ans, int i, int multiplier) {
+		if (i == -1) {
+			return ans;
+		}
+
+		ans = ans + multiplier * Integer.parseInt(String.valueOf(str.charAt(i)));
+
+		return convertInt(str, ans, i - 1, multiplier * 10);
 	}
 }
