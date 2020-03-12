616a39fa48d353566fe49ba264dad959431de2d5
Vishal1003 <vjha10032001@gmail.com>
2020-03-12 23:42:38 +0530

    DAY 7

--- Recursion/recursion3.java
+++ Recursion/recursion3.java
@@ -2,10 +2,14 @@ package Recursion;
 
 public class recursion3 {
 	public static void main(String[] args) {
-		String str = "123456";
+		String str1 = "123456";
+		String str2 = "654421";
+
+		System.out.println(isReverse(str1, str2, 0));
 
 //		System.out.println(printSum(str, 0, 0));
-		System.out.println(convertInt(str, 0, str.length()-1, 1));
+//		System.out.println(convertInt(str, 0, str.length() - 1, 1));
+
 	}
 
 	public static int printSum(String str, int sum, int i) {
@@ -27,5 +31,20 @@ public class recursion3 {
 		ans = ans + multiplier * Integer.parseInt(String.valueOf(str.charAt(i)));
 
 		return convertInt(str, ans, i - 1, multiplier * 10);
+	}
+
+	public static boolean isReverse(String str1, String str2, int i) {
+
+		if (str1.length() != str2.length())
+			return false;
+
+		if (i == str1.length()) {
+			return true;
+		}
+
+		if (str1.charAt(i) == str2.charAt(str2.length() - 1 - i))
+			return isReverse(str1, str2, i + 1);
+
+		return false;
 	}
 }
