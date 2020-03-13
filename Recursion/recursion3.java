2a2b3be8fe573102f6e17bacc3b2d6492997c9be
Vishal1003 <vjha10032001@gmail.com>
2020-03-13 17:49:08 +0530

    DAY 8

--- Recursion/recursion3.java
+++ Recursion/recursion3.java
@@ -4,9 +4,14 @@ public class recursion3 {
 	public static void main(String[] args) {
 		String str1 = "123321";
 		String str2 = "654421";
+		String str = "xfxxxgi";
 
 //		System.out.println(isReverse(str1, str2, 0));
-		System.out.println(isPalindrome(str1, 0));
+//		System.out.println(isPalindrome(str1, 0));
+//		System.out.println(replaceD(str, 0));
+//		System.out.println(reduceD(str, 0));
+
+		System.out.println(movesX(str, 0, str.length()));
 
 //		System.out.println(printSum(str, 0, 0));
 //		System.out.println(convertInt(str, 0, str.length() - 1, 1));
@@ -59,5 +64,44 @@ public class recursion3 {
 		}
 
 		return false;
+	}
+
+	public static String replaceD(String str, int i) {
+
+		if (i == str.length() - 1 || i == str.length()) {
+			return str;
+		}
+
+		if (str.charAt(i) == str.charAt(i + 1)) {
+			str = str.substring(0, i + 1) + "*" + str.substring(i + 1);
+		}
+
+		return replaceD(str, i + 1);
+
+	}
+
+	public static String reduceD(String str, int i) {
+		if (i == str.length() - 1 || i == str.length())
+			return str;
+
+		if (str.charAt(i) == str.charAt(i + 1)) {
+			str = str.substring(0, i) + str.substring(i + 1);
+		}
+
+		return reduceD(str, i + 1);
+	}
+
+	public static String movesX(String str, int i, int length) {
+
+		if (i == length)
+			return str;
+
+		if (str.charAt(i) == 'x') {
+			str = str.substring(0, i) + str.substring(i + 1) + 'x';
+			return movesX(str, i , length);
+		}
+
+		return movesX(str, i + 1, length);
+
 	}
 }
