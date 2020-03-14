af29f73c08fc363bba051427c25d648190f9dd98
Vishal1003 <vjha10032001@gmail.com>
2020-03-14 13:38:51 +0530

    DAY 9

--- Recursion_Course/recursion_get.java
+++ Recursion_Course/recursion_get.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class recursion_get {
 
 	public static void main(String[] args) {
-		
-		System.out.println(getSS("abc"));
-		
+
+//		System.out.println(getSS("abc"));
+//		System.out.println(getSSAscii("ab"));
+		System.out.println(permutation("abc"));
+
 	}
 
 	public static ArrayList<String> getSS(String str) {
@@ -32,6 +34,53 @@ public class recursion_get {
 
 		return mr;
 
+	}
+
+	public static ArrayList<String> getSSAscii(String str) {
+
+		if (str.length() == 0) {
+			ArrayList<String> br = new ArrayList<String>();
+			br.add("");
+			return br;
+		}
+
+		char ch = str.charAt(0);
+		String ros = str.substring(1);
+
+		ArrayList<String> rr = getSSAscii(ros);
+
+		ArrayList<String> mr = new ArrayList<String>();
+
+		for (String val : rr) {
+			mr.add(val);
+			mr.add(val + ch);
+			mr.add(val + (int) ch);
+		}
+
+		return mr;
+	}
+
+	public static ArrayList<String> permutation(String str) {
+		if (str.length() == 0) {
+			ArrayList<String> br = new ArrayList<String>();
+			br.add("");
+			return br;
+		}
+
+		char ch = str.charAt(0);
+		String ros = str.substring(1);
+
+		ArrayList<String> rr = permutation(ros);
+
+		ArrayList<String> mr = new ArrayList<String>();
+
+		for (String val : rr) {
+			for (int i = 0; i <= val.length(); i++) {
+				mr.add(val.substring(0, i) + ch + val.substring(i));
+			}
+		}
+
+		return mr;
 	}
 
 }
