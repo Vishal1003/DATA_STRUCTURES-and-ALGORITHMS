c60fd756741a2548d6454598296ec346460a4c37
Vishal1003 <vjha10032001@gmail.com>
2020-03-17 23:01:50 +0530

    DAY 13

--- Recursion_Course/recursion_print.java
+++ Recursion_Course/recursion_print.java
@@ -3,8 +3,40 @@ package Recursion_Course;
 public class recursion_print {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		printASCII("abc", "");
+//		printSS("abc", "");
+
+	}
+
+//	print subSequences
+	public static void printSS(String ques, String ans) {
+		if (ques.length() == 0) {
+			System.out.println(ans);
+			return;
+		}
+
+		char ch = ques.charAt(0);
+		String roq = ques.substring(1);
+
+		printSS(roq, ans + ch);
+		printSS(roq, ans);
+
+	}
+
+//	print ASCII subSeq
+	public static void printASCII(String str, String ans) {
+		if (str.length() == 0) {
+			System.out.println(ans);
+			return;
+		}
+
+		char ch = str.charAt(0);
+		String roq = str.substring(1);
+
+		printASCII(roq, ans + ch);
+		printASCII(roq, ans);
+		printASCII(roq, ans + (int) ch);
 	}
 
 }
