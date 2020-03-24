f3aefe10cead59c76930db37c24996948c169bbe
Vishal1003 <vjha10032001@gmail.com>
2020-03-24 16:27:13 +0530

    Day 21

--- Dynamic_Stack_Queue/DSClient.java
+++ Dynamic_Stack_Queue/DSClient.java
@@ -15,9 +15,12 @@ public class DSClient {
 //		ds.display();
 
 //		displayReverse(ds);
-		DynamicStack helper = new DynamicStack();
-		actualReverse(ds, helper);
-		ds.display();
+//		DynamicStack helper = new DynamicStack();
+//		actualReverse(ds, helper);
+//		ds.display();
+
+		int[] arr = { 50, 30, 20, 60, 45, 40, 70, 65 };
+		nextGreaterElement(arr);
 
 	}
 
@@ -53,12 +56,55 @@ public class DSClient {
 
 		int temp = s.pop();
 		helper.push(temp);
-		
+
 		actualReverse(s, helper);
 		return;
 
 	}
-	
-	
 
+//	nextGreater Element in array
+	public static void nextGreaterElement(int[] arr) throws Exception {
+
+		DynamicStack s = new DynamicStack();
+
+		for (int i = 0; i < arr.length; i++) {
+
+			while (!s.isEmpty() && s.peek() < arr[i]) {
+				System.out.println(s.pop() + "->" + arr[i]);
+			}
+
+			s.push(arr[i]);
+		}
+
+		while (!s.isEmpty()) {
+			System.out.println(s.pop() + "-> -1");
+		}
+
+	}
+
+//	nextGreater Element Index wise
+	public static int[] nextGreaterIndex(int[] arr) throws Exception {
+
+		int[] ans = new int[arr.length];
+		DynamicStack s = new DynamicStack();
+
+		for (int i = 0; i < arr.length; i++) {
+
+			while (!s.isEmpty() && s.peek() < arr[i]) {
+				ans[s.peek()] = arr[i];
+			}
+
+			s.push(arr[i]);
+
+		}
+
+		while (!s.isEmpty()) {
+			ans[s.pop()] = -1;
+		}
+
+		return ans;
+
+	}
+
+	
 }
