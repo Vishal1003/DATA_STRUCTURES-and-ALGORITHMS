36606476ae77ed777912631806ea5383ca8ad399
Vishal1003 <vjha10032001@gmail.com>
2020-03-24 11:28:30 +0530

    Day 21

--- Dynamic_Stack_Queue/DSClient.java
+++ Dynamic_Stack_Queue/DSClient.java
@@ -13,12 +13,15 @@ public class DSClient {
 		ds.push(60);
 		ds.push(70);
 //		ds.display();
-		
-		
-		displayReverse(ds);
+
+//		displayReverse(ds);
+		DynamicStack helper = new DynamicStack();
+		actualReverse(ds, helper);
+		ds.display();
 
 	}
 
+//	display reverse of the Stack
 	public static void displayReverse(DynamicStack s) throws Exception {
 
 		if (s.size() == 0) {
@@ -26,12 +29,36 @@ public class DSClient {
 		}
 
 		int temp = s.pop();
-
 		displayReverse(s);
 
 		System.out.println(temp);
 		s.push(temp);
 
 	}
+
+//	reverse the original stack
+	public static void actualReverse(DynamicStack s, DynamicStack helper) throws Exception {
+
+		if (s.size() == 0) {
+
+			if (helper.size() == 0) {
+				return;
+			}
+			int temph = helper.pop();
+			actualReverse(s, helper);
+
+			s.push(temph);
+			return;
+		}
+
+		int temp = s.pop();
+		helper.push(temp);
+		
+		actualReverse(s, helper);
+		return;
+
+	}
+	
+	
 
 }
