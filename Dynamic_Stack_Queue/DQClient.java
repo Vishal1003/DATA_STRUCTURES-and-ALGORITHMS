dcf85dcd56f0f6d67a64a3fd6089a998a71a814d
Vishal Jha <44357051+Vishal1003@users.noreply.github.com>
2020-03-25 12:01:46 +0530

    DAY 21

--- Dynamic_Stack_Queue/DQClient.java
+++ Dynamic_Stack_Queue/DQClient.java
@@ -19,9 +19,34 @@ public class DQClient {
 
 		dq.display();
 
+	}
 
+	public static void dispReverse(DynamicQueue q, int count) throws Exception {
+		if (count == q.size()) {
+			return;
+		}
+
+		int temp = q.dequeue();
+		q.enqueue(temp);
+
+		dispReverse(q, count + 1);
+
+		System.out.println(temp);
 
 	}
 
+	public static void actualReverse(DynamicQueue q) throws Exception {
+		if (q.isEmpty()) {
+			return;
+		}
+
+		int temp = q.dequeue();
+
+		actualReverse(q);
+
+		q.enqueue(temp);
+
+	}
 	
+
 }
