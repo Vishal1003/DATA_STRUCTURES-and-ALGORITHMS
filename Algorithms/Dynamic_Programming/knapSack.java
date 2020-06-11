bd92c3f2c3e9c0cc4943459eb1ae55c78f8b5057
Vishal Jha <44357051+Vishal1003@users.noreply.github.com>
2020-06-11 14:55:02 +0530

    knapsack dp

--- Algorithms/Dynamic_Programming/knapSack.java
+++ Algorithms/Dynamic_Programming/knapSack.java
@@ -3,8 +3,26 @@ package Dynamic_Programming;
 public class knapSack {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	}
+
+
+//	knapsack recursive
+	public static int knapsack(int[] wt, int[] val, int w, int n) {
+		if (n == 0 || w == 0) {
+			return 0;
+		}
+
+		if (wt[n - 1] <= w) {
+			int in = val[n - 1] + knapsack(wt, val, w - val[n - 1], n - 1);
+			int out = knapsack(wt, val, w, n - 1);
+
+			return Math.max(in, out);
+		}
+
+		else {
+			return knapsack(wt, val, w, n - 1);
+		}
 	}
 
 }
